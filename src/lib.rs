059419b6a8d4f2e1d935962444574ccc6ddbd9c1
kwklly <yeonlee@i.ewha.ac.kr>
2021-09-19 16:29:33 +0900

    update: update near-sdk

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,36 @@
-use borsh::{BorshDeserialize, BorshSerialize};
-use near_sdk::collections::Map;
-use near_sdk::json_types::{Base58PublicKey, U128};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LookupMap;
 use near_sdk::{
-    env, ext_contract, near_bindgen, AccountId, Balance, Promise, PromiseResult, PublicKey,
+    env, ext_contract, near_bindgen, AccountId, Balance, BorshStorageKey, Gas, PanicOnDefault,
+    Promise, PromiseResult, PublicKey,
 };
 
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[near_bindgen]
-#[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct LinkDrop {
-    pub accounts: Map<PublicKey, Balance>,
-    pub nft_accounts: Map<PublicKey, String>,
+#[derive(PanicOnDefault, BorshDeserialize, BorshSerialize)]
+pub struct NearYou {
+    pub nft_contract: AccountId,
+    pub accounts: LookupMap<PublicKey, Balance>,
+    pub nft_accounts: LookupMap<PublicKey, String>,
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Accounts,
+    NftAccounts,
 }
 
 /// Access key allowance for linkdrop keys.
 const ACCESS_KEY_ALLOWANCE: u128 = 1_000_000_000_000_000_000_000_000;
 
 /// Gas attached to the callback from account creation.
-pub const ON_CREATE_ACCOUNT_CALLBACK_GAS: u64 = 20_000_000_000_000;
+pub const ON_CREATE_ACCOUNT_CALLBACK_GAS: Gas = Gas(20_000_000_000_000);
 
 /// Indicates there are no deposit for a callback for better readability.
 const NO_DEPOSIT: u128 = 0;
-const NFT_TRANSFER_GAS: u64 = 20_000_000_000_000;
+const NFT_TRANSFER_GAS: Gas = Gas(20_000_000_000_000);
 
 #[ext_contract(ext_self)]
 pub trait ExtLinkDrop {
-    /// Callback after plain account creation.
-    fn on_account_created(&mut self, predecessor_account_id: AccountId, amount: U128) -> bool;
-
     /// Callback after creating account and claiming linkdrop.
     fn on_account_created_and_claimed(&mut self, nft_id: String) -> bool;
 }
@@ -47,9 +48,18 @@ fn is_promise_success() -> bool {
 }
 
 #[near_bindgen]
-impl LinkDrop {
+impl NearYou {
+    #[init]
+    pub fn new(nft_contract: AccountId) -> Self {
+        Self {
+            nft_contract,
+            accounts: LookupMap::new(StorageKey::Accounts),
+            nft_accounts: LookupMap::new(StorageKey::NftAccounts),
+        }
+    }
+
     #[payable]
-    pub fn send(&mut self, public_key: Base58PublicKey, nft_id: String) -> Promise {
+    pub fn send(&mut self, public_key: PublicKey, nft_id: String) -> Promise {
         assert!(
             env::attached_deposit() > ACCESS_KEY_ALLOWANCE,
             "Attached deposit must be greater than ACCESS_KEY_ALLOWANCE"
@@ -67,13 +77,11 @@ impl LinkDrop {
             pk,
             ACCESS_KEY_ALLOWANCE,
             env::current_account_id(),
-            b"claim,create_account_and_claim".to_vec(),
+            (&"claim,create_account_and_claim").to_string(),
         )
     }
 
     pub fn claim(&mut self, account_id: AccountId) -> Promise {
-        let nft_contract_name: String = String::from("nft_mint_test.testnet");
-
         assert_eq!(
             env::predecessor_account_id(),
             env::current_account_id(),
@@ -90,8 +98,8 @@ impl LinkDrop {
             .expect("Unexpected public key");
 
         Promise::new(env::current_account_id()).delete_key(env::signer_account_pk());
-        Promise::new(nft_contract_name).function_call(
-            b"nft_transfer".to_vec(),
+        Promise::new(self.nft_contract.clone()).function_call(
+            (&"nft_transfer").to_string(),
             format!(
                 "{{\"receiver_id\": \"{}\", \"token_id\": \"{}\"}}",
                 account_id, nft_id
@@ -106,9 +114,8 @@ impl LinkDrop {
     pub fn create_account_and_claim(
         &mut self,
         new_account_id: AccountId,
-        new_public_key: Base58PublicKey,
+        new_public_key: PublicKey,
     ) -> Promise {
-        let nft_contract_name: String = String::from("nft_mint_test.testnet");
         assert_eq!(
             env::predecessor_account_id(),
             env::current_account_id(),
@@ -129,17 +136,19 @@ impl LinkDrop {
             .remove(&env::signer_account_pk())
             .expect("Unexpected public key");
 
+        let nft_contract = format!(".{}", &env::current_account_id());
         let new_new_account_id = new_account_id
             .clone()
-            .replace(".testnet", ".createclaim8.testnet");
-        Promise::new(new_new_account_id.clone())
+            .to_string()
+            .replace(".testnet", &nft_contract);
+        Promise::new(AccountId::new_unchecked(new_new_account_id.clone()))
             .create_account()
             .add_full_access_key(new_public_key.into())
             .transfer(amount);
 
-        Promise::new(nft_contract_name)
+        Promise::new(self.nft_contract.clone())
             .function_call(
-                b"nft_transfer".to_vec(),
+                (&"nft_transfer").to_string(),
                 format!(
                     "{{\"receiver_id\": \"{}\", \"token_id\": \"{}\"}}",
                     new_new_account_id, nft_id
@@ -150,50 +159,10 @@ impl LinkDrop {
             )
             .then(ext_self::on_account_created_and_claimed(
                 nft_id,
-                &env::current_account_id(),
+                env::current_account_id(),
                 NO_DEPOSIT,
                 ON_CREATE_ACCOUNT_CALLBACK_GAS,
             ))
-    }
-
-    /// Create new account without linkdrop and deposit passed funds (used for creating sub accounts directly).
-    #[payable]
-    pub fn create_account(
-        &mut self,
-        new_account_id: AccountId,
-        new_public_key: Base58PublicKey,
-    ) -> Promise {
-        assert!(
-            env::is_valid_account_id(new_account_id.as_bytes()),
-            "Invalid account id"
-        );
-        let amount = env::attached_deposit();
-        Promise::new(new_account_id)
-            .create_account()
-            .add_full_access_key(new_public_key.into())
-            .transfer(amount)
-            .then(ext_self::on_account_created(
-                env::predecessor_account_id(),
-                amount.into(),
-                &env::current_account_id(),
-                NO_DEPOSIT,
-                ON_CREATE_ACCOUNT_CALLBACK_GAS,
-            ))
-    }
-
-    /// Callback after executing `create_account`.
-    pub fn on_account_created(&mut self, predecessor_account_id: AccountId, amount: U128) -> bool {
-        assert_eq!(
-            env::predecessor_account_id(),
-            env::current_account_id(),
-            "Callback can only be called from the contract"
-        );
-        let creation_succeeded = is_promise_success();
-        if !creation_succeeded {
-            // In case of failure, send funds back.
-            Promise::new(predecessor_account_id).transfer(amount.into());
-        }
-        creation_succeeded
     }
 
     /// Callback after execution `create_account_and_claim`.
@@ -214,7 +183,7 @@ impl LinkDrop {
     }
 
     /// Returns the balance associated with given key.
-    pub fn get_key_balance(&self, key: Base58PublicKey) -> String {
+    pub fn get_key_balance(&self, key: PublicKey) -> String {
         self.nft_accounts
             .get(&key.into())
             .expect("Key is missing")
